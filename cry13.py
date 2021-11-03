533a4cac8e0265651378ece7208712339fa9083e
EvolutionX <92982390+Masoudtaghdir@users.noreply.github.com>
2021-11-03 08:39:50 +0330

    Delete cry13.py

--- cry13.py
+++
@@ -1,9 +0,0 @@
-def rot13(text):
-    trans = text.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" , "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM")
-    text = text.translate(trans)
-    return text
-
-while True :    
-    text = input("text : ")
-    text = rot13(text)
-    print(text)
